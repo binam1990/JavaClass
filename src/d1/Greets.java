373b5f6b773d208744ea07e6d97cb46ce75e2820
binam1990 <pranavprachi20@gmail.com>
2021-07-15 19:49:42 -0700

    bimal added few code

--- src/d1/Greets.java
+++ src/d1/Greets.java
@@ -5,7 +5,9 @@ public class Greets {
 	public static void main(String[] args) {
 		System.out.println("hello good evening");
 		System.out.println("how are you?");
-		System.out.println("how is everything?");
+		
+		
+		System.out.println("hey are you enjoying.......");
 		
 		
 	}
