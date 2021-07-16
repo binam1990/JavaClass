44ac2b1efd07f2b8891ebbd916e34eacf3429fdc
Santosh Lal Karna <santosh.karna@dryicesolutions.net>
2021-07-16 08:22:01 +0545

    santosh added code

---
+++ src/d1/Greets.java
@@ -0,0 +1,14 @@
+package d1;
+
+public class Greets {
+
+	public static void main(String[] args) {
+		System.out.println("hello good evening");
+		System.out.println("how are you?");
+		System.out.println("how is everything?");
+		
+		
+	}
+	}
+
+
