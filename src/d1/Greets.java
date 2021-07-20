4a5662a47163911f856a028171365b624c09b730
binam1990 <pranavprachi20@gmail.com>
2021-07-19 19:27:44 -0700

    refactored code

--- src/d1/Greets.java
+++ src/d1/Greets.java
@@ -5,11 +5,7 @@ public class Greets {
 	public static void main(String[] args) {
 		System.out.println("hello good evening");
 		System.out.println("how are you?");
-			
-		
 		System.out.println("hello helo hello are you listening to me...");
-		
-	}
-	}
 
-
+	}
+}
